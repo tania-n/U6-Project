1b257e14724bf23be68d190bc358d3775ee5d4ca
tania-n <137098@epsstudents.org>
2025-01-21 14:22:29 -0500

    Create Deck.java

---
+++ Deck.java
@@ -0,0 +1,8 @@
+public class Deck{ 
+
+    private Card[] cards; 
+    private int size; // number of cards that have not been dealt with 
+
+    public Deck(String[] rank; String[] suit; int[] pointValue)
+
+}

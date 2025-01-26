6a1fbac087cd4637992aaec483f524525e67968d
tania-n <137098@epsstudents.org>
2025-01-26 03:46:33 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,7 @@
 public class Main{
     public static void main(String[] args){
-        //CardGameGUI gui = new CardGameGUI(new ElevensBoard());
-        //gui.displayGame(); 
-        
-        //Card c1 = new Card("K", "Spades", 5);
-        //System.out.println(c1); 
-        /*String[ ] ranks = {"A", "K", "Q"};
-        String[ ] suits = {"diamond", "heart"};
-        int[ ] pointValues = {1, 2, 3};
-        
-        Deck d1 = new Deck(ranks, suits, pointValues);
-        
-        d1.deal(); 
-        d1.deal(); 
-        System.out.println(d1); */
-        
-        /*System.out.println(d1.deal());
-        System.out.println(d1.deal());
-        System.out.println(d1.deal());
-        System.out.println(d1.deal());
-        System.out.println(d1.deal());
-        System.out.println(d1.deal());*/
+        CardGameGUI gui = new CardGameGUI(new ElevensBoard());
+        gui.displayGame(); 
         
     }
 }

74a6430773f82c972d1a5ff4c6166752aae6b1f1
tania-n <137098@epsstudents.org>
2025-01-26 19:57:11 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -7,21 +7,24 @@ public class Card{
         rank = r; 
         suit = s; 
         pointValue = p; 
-        
     } 
 
+    // accessor for rank
     public String getRank(){ 
         return rank; 
     } 
 
+    // accessor for suit  
     public String getSuit(){ 
         return suit; 
     } 
 
+    // accessor for pointValue 
     public int getPointValue(){ 
         return pointValue; 
     } 
 
+    // if the rank, suit, and pointValue equal the ones from Card c, then it will return true 
     public boolean equals(Card c){ 
         if(this.rank.equals(c.rank) && this.suit.equals(c.suit) && this.pointValue == c.pointValue){ 
             return true; 
@@ -31,11 +34,8 @@ public class Card{
         }
     } 
 
+    // puts the rank, suit, and pointValue in the proper format 
     public String toString(){ 
         return rank + " of " + suit + " (point value = " + pointValue + ")"; 
     }
-
-
-
-
 }
